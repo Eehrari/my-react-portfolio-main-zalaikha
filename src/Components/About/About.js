f91d208ca25b9fc9dc93ba0bef5955793e114f7b
Ehsan Ehrari <ehsan.ehrari@codetoinspire.org>
2023-12-04 10:45:32 +0430

    Initial commit

---
+++ src/Components/About/About.js
@@ -0,0 +1,35 @@
+// import { useTranslation } from "react-i18next";
+import './about.css';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import AboutContent from "./AboutContent";
+import AboutTitleSection from "./AboutTitleSection";
+import AboutImage from "./AboutImage";
+const About = ({ isDarkMode }) => {
+  //  const [t] = useTranslation("global") ;
+  useEffect(() => {
+    AOS.init(
+      {
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+      }
+    );
+
+  }, []);
+
+  return (
+    <section id="about" className="about">
+      <div className="container">
+        <AboutTitleSection />
+        <div className="row">
+          <AboutImage isDarkMode={isDarkMode} />
+          <AboutContent />
+        </div>
+      </div>
+    </section>
+  )
+}
+export default About;
---
+++ src/Components/About/AboutImage.js
@@ -0,0 +1,11 @@
+import profileImg from '../../assets/images/profile-image.png';
+// import DarkProfileImg from '../../assets/images/darkmode-hero-bg.png';
+import AnimatedSvg from '../AnimatedSVG';
+const AboutImage = ({ isDarkMode }) => {
+    return (
+        <div className="col-lg-4" data-aos="fade-right">
+            {isDarkMode ? <AnimatedSvg></AnimatedSvg> : <img src={profileImg} className="img-fluid" alt=""></img>}
+        </div>
+    )
+}
+export default AboutImage;
